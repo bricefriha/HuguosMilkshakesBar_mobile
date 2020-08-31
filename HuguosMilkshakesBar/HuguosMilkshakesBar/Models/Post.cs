1d59a9d5c588f42b7958aa8822754b2ee06657aa
Brice Friha <brice.friha@aol.fr>
2020-08-31 15:41:07 +0200

    I made the post model

---
+++ HuguosMilkshakesBar/HuguosMilkshakesBar/Models/Post.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace HuguosMilkshakesBar.Models
+{
+    class Post
+    {
+        [JsonProperty("headline")]
+        public string Headline { get; set; }
+        [JsonProperty("content")]
+        public string Content { get; set; }
+        [JsonProperty("createdDate")]
+        public DateTime CreatedDate { get; set; }
+    }
+}

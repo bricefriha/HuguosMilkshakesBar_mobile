10ecb7ed1e1bd24ee6ac3e1f0be4b4b13ab3f2d0
Brice Friha <brice.friha@aol.fr>
2020-09-09 18:49:49 +0200

    Make sure to get the previous session back

--- HuguosMilkshakesBar/HuguosMilkshakesBar/App.xaml.cs
+++ HuguosMilkshakesBar/HuguosMilkshakesBar/App.xaml.cs
@@ -1,7 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using CustardApi.Objects;
 using HuguosMilkshakesBar.Models;
 using HuguosMilkshakesBar.Views;
+using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -18,11 +21,46 @@ namespace HuguosMilkshakesBar
             // Set the web service
             WService = new Service("192.168.1.67", 3000);
 
-            MainPage = new AppShell();
+            MainPage = new Page();
         }
 
-        protected override void OnStart()
+        protected override async void OnStart()
         {
+
+            await FetchPreviousToken();
+
+            MainPage = new AppShell();
+
+
+        }
+        /// <summary>
+        /// Fetch the token of the previous session if this one exist
+        /// </summary>
+        /// <returns></returns>
+        private static async Task FetchPreviousToken()
+        {
+            try
+            {
+                // Get the token of the previous session
+                var Token = await SecureStorage.GetAsync("oauth_token");
+
+
+                // If this token exist
+                if (!string.IsNullOrEmpty(Token))
+                {
+                    // Set the header
+                    IDictionary<string, string> headers = new Dictionary<string, string>();
+                    // Fetch the user token
+                    headers.Add("Authorization", "Bearer " + Token);
+
+                    App.currentUser = await WService.ExecuteGet<User>("users", null, headers);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            
         }
 
         protected override void OnSleep()

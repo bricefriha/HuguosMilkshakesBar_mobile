ef6a2c7897c77941ee658b8cee9468da23257e03
Brice Friha <brice.friha@aol.fr>
2020-09-18 17:52:19 +0200

    Authorise http for iOS and Android

---
+++ HuguosMilkshakesBar/HuguosMilkshakesBar/Models/Milkshake.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+using Xamarin.Forms;
+
+namespace HuguosMilkshakesBar.Models
+{
+    public class Milkshake
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+        [JsonProperty("picture")]
+        public string Picture { get; set; }
+        [JsonProperty("description")]
+        public string Description { get; set; }
+        [JsonProperty("price")]
+        public string Price { get; set; }
+        //public Uri Picture
+        //{ 
+        //    get {
+        //        return new Uri(PictureString);
+        //    }
+        //}
+    }
+}
---
+++ HuguosMilkshakesBar/HuguosMilkshakesBar/Viewmodels/CatalogViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Threading.Tasks;
+using HuguosMilkshakesBar.Models;
+using MvvmHelpers;
+
+namespace HuguosMilkshakesBar.Viewmodels
+{
+    public class CatalogViewModel : BaseViewModel 
+    {
+        private ObservableCollection<Milkshake> _milkshakes; 
+        public ObservableCollection<Milkshake> Milkshakes
+        {
+            set
+            {
+                _milkshakes = value;
+                OnPropertyChanged();
+            }
+            get
+            {
+                return _milkshakes;
+            }
+        }
+        public CatalogViewModel ()
+        {
+            // Get all the milkshakes
+            _milkshakes = new ObservableCollection<Milkshake>(Task< Collection < Milkshake >>.Run( async () => await App.WService.ExecuteGet<Collection<Milkshake>>("milkshakes/")).Result);
+        }
+    }
+}
